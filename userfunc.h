13d6f6a78ee4ef8171c9e78d69dbed497d283fa7
MARVELLGG <115549649+MARVELLGG@users.noreply.github.com>
2025-01-10 23:18:50 +0700

    Update userfunc.h

--- userfunc.h
+++ userfunc.h
@@ -356,7 +356,7 @@ void GrowtopiaBot::OnTalkBubble(int netID, string bubbleText, int type, int numb
 	}
 	if (bubbleText.find("!respawn") != string::npos)
 	{
-		GrowtopiaBot::respawn();
+		respawn();
 	}
 	if (bubbleText.find("!nfollow") != string::npos)
 	{
