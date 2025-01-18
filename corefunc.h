8e3e1667bf83efa9b62d4697774e8175ae6306b5
MARVELLGG <115549649+MARVELLGG@users.noreply.github.com>
2025-01-18 15:08:01 +0700

    Add files via upload

--- corefunc.h
+++ corefunc.h
@@ -220,37 +220,47 @@ public:
 		connectClient(SERVER_HOST, SERVER_PORT);
 	}
 
-	void connectClient(string hostName, int port)
-	{
-		cout << "Connecting bot to " << hostName << ":" << port << endl;
-		client = enet_host_create(NULL /* create a client host */,
-			10 /* only allow 1 outgoing connection */,
-			10 /* allow up 2 channels to be used, 0 and 1 */,
-			0 /* 56K modem with 56 Kbps downstream bandwidth */,
-			0 /* 56K modem with 14 Kbps upstream bandwidth */);
-		client->usingNewPacket = false;
-		if (client == NULL)
-		{
-			cout << "An error occurred while trying to create an ENet client host.\n";
-			
-			exit(EXIT_FAILURE);
-		}
-		ENetAddress address;
+	void connectClient(string hostName, int port) {
+    // Daftar IP lokal
+    string localIPs[] = {
+        "192.168.1.8",
+        "192.168.1.4",
+    };
 
-		client->checksum = enet_crc32;
-		enet_host_compress_with_range_coder(client);
-		enet_address_set_host(&address, hostName.c_str());
-		address.port = port;
+    // Iterasi melalui setiap IP lokal
+    for (const string& localIP : localIPs) {
+        cout << "Connecting bot to " << hostName << ":" << port << " using IP: " << localIP << endl;
 
-		/* Initiate the connection, allocating the two channels 0 and 1. */
-		peer = enet_host_connect(client, &address, 10, 0);
-		if (peer == NULL)
+        // Bind ke IP lokal tertentu
+        ENetAddress bindAddress;
+        enet_address_set_host(&bindAddress, localIP.c_str());
+        bindAddress.port = 0;  // Pilih port random untuk koneksi keluar
+
+        // Buat ENetHost untuk setiap IP lokal
+        ENetHost* client = enet_host_create(&bindAddress, 10 /* max connections */, 10 /* max channels */, 0, 0);
+        
+        // Cek apakah host berhasil dibuat
+        if (client == NULL) {
+            cout << "Failed to create ENetHost for IP: " << localIP << endl;
+            continue;  // Coba IP lain jika gagal
+        }
+
+        client->usingNewPacket = false;
+        client->checksum = enet_crc32;
+        enet_host_compress_with_range_coder(client);
+
+        // Buat koneksi ke server
+        ENetAddress address;
+        enet_address_set_host(&address, hostName.c_str());
+        address.port = port;
+
+        peer = enet_host_connect(client, &address, 2, 0);
+        if (peer == NULL)
 		{
 			cout << "No available peers for initiating an ENet connection.\n";
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);
 		peer2 = enet_host_connect(client, &address, 10, 0);
 		if (peer2 == NULL)
 		{
@@ -258,7 +268,6 @@ public:
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);		
 	peer3 = enet_host_connect(client, &address, 10, 0);
 		if (peer3 == NULL)
 		{
@@ -266,7 +275,6 @@ public:
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);
 	peer4 = enet_host_connect(client, &address, 10, 0);
 		if (peer4 == NULL)
 		{
@@ -274,7 +282,6 @@ public:
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);
 	peer5 = enet_host_connect(client, &address, 10, 0);
 		if (peer5 == NULL)
 		{
@@ -282,7 +289,6 @@ public:
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);
 	peer6 = enet_host_connect(client, &address, 10, 0);
 		if (peer6 == NULL)
 		{
@@ -298,7 +304,6 @@ public:
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);
 	peer8 = enet_host_connect(client, &address, 10, 0);
 		if (peer8 == NULL)
 		{
@@ -306,7 +311,6 @@ public:
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);
 	peer9 = enet_host_connect(client, &address, 10, 0);
 		if (peer9 == NULL)
 		{
@@ -314,7 +318,6 @@ public:
 			
 			exit(EXIT_FAILURE);
 		}
-		enet_host_flush(client);
 	peer10 = enet_host_connect(client, &address, 10, 0);
 		if (peer10 == NULL)
 		{
@@ -323,9 +326,12 @@ public:
 			exit(EXIT_FAILURE);
 		}
 		enet_host_flush(client);
+		cout << "Bot connected using IP: " << localIP << " with multiple peers." << endl;
+		
+	}
 	}
 	/******************* enet core *********************/
-
+,
 
 
 	/*************** sender sutff **************/
