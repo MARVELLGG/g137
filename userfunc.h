13b907725902a40d32d2a0020429427013d703d5
MARVELLGG <115549649+MARVELLGG@users.noreply.github.com>
2025-01-19 14:04:03 +0700

    Add files via upload

--- userfunc.h
+++ userfunc.h
@@ -1381,9 +1381,10 @@ SendPacket(3, "action|join_request\nname|" + worldName, peer12); // MARRKS
 SendPacket(3, "action|join_request\nname|" + worldName, peer13); // MARRKS
 SendPacket(3, "action|join_request\nname|" + worldName, peer14); // MARRKS
             // Tunggu hingga benar-benar masuk ke dunia
-objects.clear();
+
 msgloop();
             cout << currentWorld << "; " << worldName << endl;
+            objects.clear();
             return;
         }
         }
